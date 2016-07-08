3cebd7410d9fdb4bfdf45b0b6005411055282c22
Juliette3529 <Juliette@saint-jalme.fr>
2016-07-08 12:10:57 +0200

    premier algo en attendant reparation du jasmine

--- anagram.js
+++ anagram.js
@@ -11,6 +11,21 @@ function Anagram(word) {
 }
 
 Anagram.prototype.matches = function (words) {
+    var wrod = word.split("").sort().join().trim() ;
+    ltabwords = words.length ;
+    for (i = 0, i <= ltabwords , i++) {
+     
+        var wrods = words[i].split("").sort().join().trim() ;
+        // je tableaute le mot, je le trie et je le remets en mot en enlevant les espaces . 
+    if  (wrods === wrod) {
+      // renvoie l'element du tableau comparé  
+          return words[i] ; 
+    } else{
+        return [""] ;
+        //sinon renvoie du viiiiiiiiide
+    }
+    };
+    
 //
 // YOUR CODE GOES HERE
 //
