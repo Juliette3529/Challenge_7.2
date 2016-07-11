c61a1e06ef46b2e62bac47bddd97bee5810c436e
Juliette3529 <Juliette@saint-jalme.fr>
2016-07-11 12:01:51 +0200

    test jasmine positif

--- anagram.js
+++ anagram.js
@@ -9,35 +9,42 @@
 function Anagram(word) {
   this.word = word ;
 };
+ 
 
-Anagram.prototype.trie = function (carotte) {
-    mot = this.carotte.toLowerCase();
-    mot = mot.split("");
-    mot = mot.sort();
-    mot = mot.join();
-    mot = mot.trim();
-    return mot;
-};
 // je tableaute le mot, je le trie et je le remets en mot en enlevant les espaces .
 
 Anagram.prototype.matches = function (words) {
     var wrod = this.trie(this.word) ;
-    var ltabwords = words.length ;
+
     var matches = [] ;
-    
-    for (var i = 0 ; i <= ltabwords ; i++) {
+    // typeof "" === 'string' 
+    //var args = Array.from(arguments);
+    //voir pour transformer les string en array
+   if (typeof words === 'string'){
+       words = arguments;
+    }
+    var ltabwords = words.length ;
+    for (var i = 0 ; i < ltabwords ; i++) {
      var Ewords = words[i];
-     var wrods = this.trie(Ewords) ;
-         
-    if  (wrods === wrod) {
+     var wrods = this.trie(Ewords) ; 
+    if  (wrods === wrod && Ewords.toUpperCase() != this.word.toUpperCase()) {
+    //&& pour verifier que mots differents 
       // renvoie l'element du tableau comparé  
-        matches[i] = Ewords ;
-        // matches = matches + matches.push(Ewords) ; 
+       // matches[i] = Ewords ;
+        matches.push(Ewords) ; 
     }
     }
     return matches;
-    
+
 //
 // YOUR CODE GOES HERE
 //
+};
+
+Anagram.prototype.trie = function (string1) {
+    var str = string1.toUpperCase();
+    str = str.split("");
+    str = str.sort();
+    str = str.join("");
+    return str;
 };
